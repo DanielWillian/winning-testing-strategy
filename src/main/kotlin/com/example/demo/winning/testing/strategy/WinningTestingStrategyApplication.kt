d20313b3bdf66f558f55e4809dfe68bf0be78b32
Daniel Willian <d.willian_sc@hotmail.com>
2024-06-23 16:52:11 -0300

    Initial commit

---
+++ src/main/kotlin/com/example/demo/winning/testing/strategy/WinningTestingStrategyApplication.kt
@@ -0,0 +1,11 @@
+package com.example.demo.winning.testing.strategy
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WinningTestingStrategyApplication
+
+fun main(args: Array<String>) {
+	runApplication<WinningTestingStrategyApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/demo/winning/testing/strategy/TestWinningTestingStrategyApplication.kt
@@ -0,0 +1,9 @@
+package com.example.demo.winning.testing.strategy
+
+import org.springframework.boot.fromApplication
+import org.springframework.boot.with
+
+
+fun main(args: Array<String>) {
+	fromApplication<WinningTestingStrategyApplication>().with(TestcontainersConfiguration::class).run(*args)
+}
